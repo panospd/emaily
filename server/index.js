f5f5d01ee68c73f29847e363ced4311bc3ddfc0b
Panos Anastasiadis <anastapd@gmail.com>
2024-04-09 22:31:56 +0100

    removed get endpoint and added passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.send({ hi: "buddy" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5001;
 
