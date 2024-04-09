391684b66072d1b766c4a5f72b72047171ab2701
Panos Anastasiadis <anastapd@gmail.com>
2024-04-09 20:59:19 +0100

    minor update

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.send({ hi: "there" });
+    return res.send({ hi: "buddy" });
 });
 
 const PORT = process.env.PORT || 5001;
