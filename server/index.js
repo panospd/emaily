0af3d09c6be14d302752c94469e130c38c4bf504
Panos Anastasiadis <anastapd@gmail.com>
2024-04-11 15:00:25 +0100

    create user on sign up if not exist

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
+
+const mongoose = require("mongoose");
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require("./routes/authRoutes")(app);
