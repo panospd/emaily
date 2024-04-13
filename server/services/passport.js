a70e366acde41d1247fab3241496d7d258518532
Panos Anastasiadis <anastapd@gmail.com>
2024-04-13 22:48:05 +0100

    replace promises to async/await in passport file

--- server/services/passport.js
+++ server/services/passport.js
@@ -24,17 +24,15 @@ passport.use(
             callbackURL: "/auth/google/callback",
             proxy: true,
         },
-        (accessToken, refreshToken, profile, done) => {
+        async (accessToken, refreshToken, profile, done) => {
             console.log(accessToken);
-            User.findOne({ googleId: profile.id }).then((existingUser) => {
-                if (!existingUser) {
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then((user) => done(null, user));
-                } else {
-                    done(null, existingUser);
-                }
-            });
+            const existingUser = await User.findOne({ googleId: profile.id });
+            if (!existingUser) {
+                const user = await new User({ googleId: profile.id }).save();
+                done(null, user);
+            } else {
+                done(null, existingUser);
+            }
         }
     )
 );
