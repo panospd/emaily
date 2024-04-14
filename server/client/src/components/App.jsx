7f45d246e28d494e6a607b4506cd2620fdc5ee58
Panos Anastasiadis <anastapd@gmail.com>
2024-04-14 21:57:41 +0100

    implement sudo routes

--- server/client/src/components/App.jsx
+++ server/client/src/components/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 function App() {
-    return <div>Hi There</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
