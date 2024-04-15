30010df95d5d52c7f25cefc263e1c8f1619f4d31
Panos Anastasiadis <anastapd@gmail.com>
2024-04-15 21:23:57 +0100

    logout user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,7 +18,7 @@ module.exports = (app) => {
 
     app.get("/api/logout", (req, res) => {
         req.logout();
-        return res.send(req.user);
+        res.redirect("/");
     });
 
     app.get("/api/current_user", (req, res) => {
