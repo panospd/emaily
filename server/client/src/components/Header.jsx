8cc8331d400661736a5fa13286a0217d72b257ca
Panos Anastasiadis <anastapd@gmail.com>
2024-04-15 07:24:10 +0100

    add left padding to logo

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -3,18 +3,22 @@ import { Component } from "react";
 class Header extends Component {
     render() {
         return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#" className="left brand-logo">
-                        Emaily
-                    </a>
-                    <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <div className="row">
+                <nav>
+                    <div className="nav-wrapper">
+                        <div className="col s12">
+                            <a href="#" className="brand-logo">
+                                Emaily
+                            </a>
+                            <ul className="right">
+                                <li>
+                                    <a>Login With Google</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         );
     }
 }
