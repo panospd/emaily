77d18175b968b7e88a55edddbeecae9f8b6eef3c
Panos Anastasiadis <anastapd@gmail.com>
2024-04-19 20:57:03 +0100

    implement express serve static files

--- server/index.js
+++ server/index.js
@@ -23,6 +23,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/payments")(app);
 
+if (process.env.NODE_ENV === "production") {
+    // express will serve up production assets (main.js or main.css for example)
+    app.use(express.static("client/dist"));
+
+    // express will serve up the index file
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    });
+}
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
